44457de7103b0f684e6da5b81f5aa203ec6c8c2e
Alfredo <alfredocubitos@yahoo.com.mx>
2019-02-10 17:07:21 +0100

    Examples changed

---
+++ README.md
@@ -0,0 +1,31 @@
+# Fork working with 0x7783 LCD devices
+
+Added the hardware specific values from the SWIFT driver by Smoke And Wires
+This works also for some Iduino 2.8'' displays
+
+Now you can work with the Adafruit examples.
+
+This library is only for the Arduino Uno
+
+
+This is a library for the Adafruit 2.8" TFT display.
+This library works with the Adafruit 2.8" TFT Breakout w/SD card
+  ----> http://www.adafruit.com/products/335
+as well as Adafruit TFT Touch Shield
+  ----> http://www.adafruit.com/products/376
+ 
+Check out the links above for our tutorials and wiring diagrams.
+These displays use 8-bit parallel to communicate, 12 or 13 pins are required
+to interface (RST is optional).
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+
+Written by Limor Fried/Ladyada for Adafruit Industries.
+MIT license, all text above must be included in any redistribution
+
+To download. click the DOWNLOADS button in the top right corner, rename the uncompressed folder Adafruit_TFTLCD. Check that the Adafruit_TFTLCD folder contains Adafruit_TFTLCD.cpp and Adafruit_TFTLCD.
+
+Place the Adafruit_TFT library folder your <arduinosketchfolder>/libraries/ folder. You may need to create the libraries subfolder if its your first library. Restart the IDE
+
+Also requires the Adafruit_GFX library for Arduino. https://github.com/adafruit/Adafruit-GFX-Library
---
+++ examples/rotationtest/rotationtest.ino
@@ -0,0 +1,225 @@
+// IMPORTANT: Adafruit_TFTLCD LIBRARY MUST BE SPECIFICALLY
+// CONFIGURED FOR EITHER THE TFT SHIELD OR THE BREAKOUT BOARD.
+// SEE RELEVANT COMMENTS IN Adafruit_TFTLCD.h FOR SETUP.
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_TFTLCD.h> // Hardware-specific library
+
+// The control pins for the LCD can be assigned to any digital or
+// analog pins...but we'll use the analog pins as this allows us to
+// double up the pins with the touch screen (see the TFT paint example).
+#define LCD_CS A3 // Chip Select goes to Analog 3
+#define LCD_CD A2 // Command/Data goes to Analog 2
+#define LCD_WR A1 // LCD Write goes to Analog 1
+#define LCD_RD A0 // LCD Read goes to Analog 0
+
+#define LCD_RESET A4 // Can alternately just connect to Arduino's reset pin
+
+// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
+// For the Arduino Uno, Duemilanove, Diecimila, etc.:
+//   D0 connects to digital pin 8  (Notice these are
+//   D1 connects to digital pin 9   NOT in order!)
+//   D2 connects to digital pin 2
+//   D3 connects to digital pin 3
+//   D4 connects to digital pin 4
+//   D5 connects to digital pin 5
+//   D6 connects to digital pin 6
+//   D7 connects to digital pin 7
+// For the Arduino Mega, use digital pins 22 through 29
+// (on the 2-row header at the end of the board).
+
+// Assign human-readable names to some common 16-bit color values:
+#define	BLACK   0x0000
+#define	BLUE    0x001F
+#define	RED     0xF800
+#define	GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+// If using the shield, all control and data lines are fixed, and
+// a simpler declaration can optionally be used:
+// Adafruit_TFTLCD tft;
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.println(F("TFT LCD test"));
+
+#ifdef USE_ADAFRUIT_SHIELD_PINOUT
+  Serial.println(F("Using Adafruit 2.8\" TFT Arduino Shield Pinout"));
+#else
+  Serial.println(F("Using Adafruit 2.8\" TFT Breakout Board Pinout"));
+#endif
+
+  tft.reset();
+
+  uint16_t identifier = tft.readID();
+
+  if(identifier == 0x9325) {
+    Serial.println(F("Found ILI9325 LCD driver"));
+  } else if(identifier == 0x9328) {
+    Serial.println(F("Found ILI9328 LCD driver"));
+  } else if(identifier == 0x7575) {
+    Serial.println(F("Found HX8347G LCD driver"));
+  } else if(identifier == 0x9341) {
+    Serial.println(F("Found ILI9341 LCD driver"));
+  } else if(identifier == 0x8357) {
+    Serial.println(F("Found HX8357D LCD driver"));
+  } else if(identifier == 0x7783){
+    Serial.println(F("Found 7783 LCD driver"));
+  } else {
+    Serial.print(F("Unknown LCD driver chip: "));
+    Serial.println(identifier, HEX);
+    Serial.println(F("If using the Adafruit 2.8\" TFT Arduino shield, the line:"));
+    Serial.println(F("  #define USE_ADAFRUIT_SHIELD_PINOUT"));
+    Serial.println(F("should appear in the library header (Adafruit_TFT.h)."));
+    Serial.println(F("If using the breakout board, it should NOT be #defined!"));
+    Serial.println(F("Also if using the breakout, double-check that all wiring"));
+    Serial.println(F("matches the tutorial."));
+    return;
+  }
+
+  tft.begin(identifier);
+
+  tft.fillScreen(BLACK);
+
+  Serial.println(F("This is a test of the rotation capabilities of the TFT library!"));
+  Serial.println(F("Press <SEND> (or type a character) to advance"));
+}
+
+void loop(void) {
+  rotatePixel();
+  rotateLine();
+  rotateFastline();
+  rotateDrawrect();
+  rotateFillrect();
+  rotateDrawcircle();
+  rotateFillcircle();
+  rotateText();
+}
+
+void rotateText() {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.setCursor(0, 30);
+    tft.setTextColor(RED);
+    tft.setTextSize(1);
+    tft.println("Hello World!");
+    tft.setTextColor(YELLOW);
+    tft.setTextSize(2);
+    tft.println("Hello World!");
+    tft.setTextColor(GREEN);
+    tft.setTextSize(3);
+    tft.println("Hello World!");
+    tft.setTextColor(BLUE);
+    tft.setTextSize(4);
+    tft.print(1234.567);
+
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
+void rotateFillcircle(void) {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.fillCircle(10, 30, 10, YELLOW);
+
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
+void rotateDrawcircle(void) {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.drawCircle(10, 30, 10, YELLOW);
+
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
+void rotateFillrect(void) {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.fillRect(10, 20, 10, 20, GREEN);
+
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
+void rotateDrawrect(void) {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.drawRect(10, 20, 10, 20, GREEN);
+
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
+void rotateFastline(void) {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.drawFastHLine(0, 20, tft.width(), RED);
+    tft.drawFastVLine(20, 0, tft.height(), BLUE);
+
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
+void rotateLine(void) {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.drawLine(tft.width()/2, tft.height()/2, 0, 0, RED);
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
+void rotatePixel(void) {
+  for (uint8_t i=0; i<4; i++) {
+    tft.fillScreen(BLACK);
+    Serial.println(tft.getRotation(), DEC);
+
+    tft.drawPixel(10,20, RED);
+    while (!Serial.available());
+    Serial.read();  Serial.read();  Serial.read();
+
+    tft.setRotation(tft.getRotation()+1);
+  }
+}
+
